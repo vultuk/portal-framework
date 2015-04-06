536301a8c1e8d0c598d71280e052ddc68659c4b5
Simon Skinner <mail@vultuk.net>
2015-04-06 11:15:59 +0100

    Booting of the portal completed

---
+++ resources/configs/app/routes.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'load' => [
+        'ticket',
+    ],
+];
--- src/Portal/Providers/PortalServiceProvider.php
+++ src/Portal/Providers/PortalServiceProvider.php
@@ -4,6 +4,13 @@ use Illuminate\Support\ServiceProvider;
 
 class PortalServiceProvider extends ServiceProvider {
 
+    protected $configFiles = [
+        'app.routes',
+    ];
+
+    protected $resourcesDirectory = '/../../../';
+
+
     /**
      * Perform post-registration booting of services.
      *
@@ -11,13 +18,18 @@ class PortalServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        dd(__DIR__);
+        // Load the config files
+        $this->mergeConfigFiles();
 
-        $this->loadViewsFrom(__DIR__.'/path/to/views', 'portal');
+        // Load the required routes
+        $this->loadPackageRoutes();
 
-        $this->loadTranslationsFrom(__DIR__.'/path/to/translations', 'courier');
+        // Load the required Views
+        $this->loadViewsFrom($this->getDirectory($this->resourcesDirectory, 'resources/views'), 'portal');
 
-        include __DIR__.'/../../resources/routes/ticket.php';
+        // Load the required language files
+        $this->loadTranslationsFrom($this->getDirectory($this->resourcesDirectory, 'resources/lang'), 'portal');
+
     }
 
     /**
@@ -28,5 +40,80 @@ class PortalServiceProvider extends ServiceProvider {
     public function register()
     {
         // TODO: Implement register() method.
+    }
+
+
+    /**
+     * @param null $source
+     * @param null $item
+     * @return string
+     */
+    private function getDirectory($source = null, $item = null)
+    {
+        // Generate the requested directory
+        return __DIR__ . $source . $item;
+    }
+
+
+    /**
+     * @return array
+     */
+    private function mergeConfigFiles()
+    {
+        // Store all the merged config files
+        $mergedConfigFiles = [];
+
+        // Merge all config files added to the class array
+        foreach ($this->configFiles as $configFile)
+        {
+            // Find the real file name
+            $fileName = $this->getDirectory($this->resourcesDirectory, 'resources/configs/' . str_replace('.', '/', $configFile) . '.php');
+
+
+            // If the config file exists then merge in, otherwise log as an error
+            if (file_exists($fileName))
+            {
+                $this->mergeConfigFrom($fileName, 'portal.'.$configFile);
+                $mergedConfigFiles[] = $configFile;
+            } else {
+                \Log::error("Couldn't merge config file.", [
+                    'package' => 'portal/framework',
+                    'configFile' => $configFile
+                ]);
+            }
+        }
+
+        return $mergedConfigFiles;
+    }
+
+    /**
+     * @return array
+     */
+    private function loadPackageRoutes()
+    {
+        // Store all the loaded route files
+        $loadedRouteFiles = [];
+
+        // Load all route files from the config file
+        foreach (config('portal.app.routes.load') as $routeFile)
+        {
+            // Find the real file name
+            $fileName = $this->getDirectory($this->resourcesDirectory, 'resources/routes/'.str_replace('.', '/', $routeFile).'.php');
+
+            // If the route file exists then merge in, otherwise log as an error
+            if (file_exists($fileName))
+            {
+                include $fileName;
+                $loadedRouteFiles[] = $loadedRouteFiles;
+            } else {
+                \Log::error("Couldn't load route file.", [
+                    'package' => 'portal/framework',
+                    'routeFile' => $routeFile
+                ]);
+            }
+
+        }
+
+        return $loadedRouteFiles;
     }
 }
